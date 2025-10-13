06ec34d9a571966897a07895979cb7d8a6962664
Santoragi <woojj1254577@naver.com>
2025-10-13 13:35:56 +0900

    init

---
+++ src/main/java/hansung/hansung_connect/HansungConnectApplication.java
@@ -0,0 +1,13 @@
+package hansung.hansung_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansungConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansungConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hansung/hansung_connect/HansungConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package hansung.hansung_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansungConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
