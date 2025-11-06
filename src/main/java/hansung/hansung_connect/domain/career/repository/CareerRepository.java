e5408bf31e4aa1fc53192fb44201dbf222413d67
goeka <goeka1337@gmail.com>
2025-11-07 05:22:17 +0900

    feat(repository): CareerRepository·LinkRepository에 사용자 기준 전체 조회 메서드 추가

--- src/main/java/hansung/hansung_connect/domain/career/repository/CareerRepository.java
+++ src/main/java/hansung/hansung_connect/domain/career/repository/CareerRepository.java
@@ -1,9 +1,13 @@
 package hansung.hansung_connect.domain.career.repository;
 
 import hansung.hansung_connect.domain.career.entity.Career;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CareerRepository extends JpaRepository<Career, Long> {
     boolean existsByUser_IdAndIsEmployedTrue(Long userId);
+
+    List<Career> findAllByUser_Id(Long userId);
+
 }
 
