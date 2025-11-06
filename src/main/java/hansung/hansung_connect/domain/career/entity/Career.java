55e57b6067ec6217875a58f00a0d3865addb7862
goeka <goeka1337@gmail.com>
2025-11-06 09:12:47 +0900

    feat(career): Career 엔티티에 근무시작연월, 종료연월 필드 추가

--- src/main/java/hansung/hansung_connect/domain/career/entity/Career.java
+++ src/main/java/hansung/hansung_connect/domain/career/entity/Career.java
@@ -1,9 +1,11 @@
 package hansung.hansung_connect.domain.career.entity;
 
+import hansung.hansung_connect.domain.career.converter.YearMonthAttributeConverter;
 import hansung.hansung_connect.domain.career.entity.enums.JobType;
 import hansung.hansung_connect.domain.user.entity.User;
 import hansung.hansung_connect.global.common.BaseEntity;
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.YearMonth;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,6 +44,14 @@ public class Career extends BaseEntity {
 
     @Column(nullable = false)
     private boolean isEmployed;
+
+    @Convert(converter = YearMonthAttributeConverter.class)
+    @Column(name = "start_ym", nullable = false, length = 7) // 시작연월 "2024-04"
+    private YearMonth startYm;
+
+    @Convert(converter = YearMonthAttributeConverter.class)
+    @Column(name = "end_ym", length = 7) // 재직중이면 null
+    private YearMonth endYm;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
